6379cb66a471c3c7d5be181b56cf3d57ead07aee
tesla95E <andybiscom@gmail.com>
2024-06-30 20:31:13 +0300

    connected gameTale with GameLogic. Rewrite some other parts

--- tic_tac_toe_app/src/Components/GameLogic.js
+++ tic_tac_toe_app/src/Components/GameLogic.js
@@ -1,24 +1,23 @@
-const tableArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-export function GameResult(tableArray) {
-  if (tableArray[0] == tableArray[1] && tableArray[0] == tableArray[2]) {
+export function GameResult(array) {
+  if (array[0] == array[1] && array[0] == array[2]) {
     return "player win";
-  } else if (tableArray[1] == tableArray[4] && tableArray[3] == tableArray[5]) {
+  } else if (array[1] == array[4] && array[3] == array[5]) {
     return "player win";
-  } else if (tableArray[6] == tableArray[7] && tableArray[6] == tableArray[8]) {
+  } else if (array[6] == array[7] && array[6] == array[8]) {
     return "player win";
-  } else if (tableArray[1] == tableArray[4] && tableArray[1] == tableArray[7]) {
+  } else if (array[1] == array[4] && array[1] == array[7]) {
     return "player win";
-  } else if (tableArray[2] == tableArray[5] && tableArray[2] == tableArray[8]) {
+  } else if (array[2] == array[5] && array[2] == array[8]) {
     return "player win";
-  } else if (tableArray[3] == tableArray[6] && tableArray[3] == tableArray[9]) {
+  } else if (array[3] == array[6] && array[3] == array[9]) {
     return "player win";
-  } else if (tableArray[1] == tableArray[5] && tableArray[1] == tableArray[9]) {
+  } else if (array[1] == array[5] && array[1] == array[9]) {
     return "player win";
-  } else if (tableArray[3] == tableArray[5] && tableArray[3] == tableArray[7]) {
+  } else if (array[3] == array[5] && array[3] == array[7]) {
     return "player win";
   } else {
     return "egal";
   }
 }
 
-console.log(GameResult(tableArray));
+export default GameResult;
--- tic_tac_toe_app/src/Components/GameTable.jsx
+++ tic_tac_toe_app/src/Components/GameTable.jsx
@@ -1,24 +1,31 @@
 import { React, useState } from "react";
-import { GameResult } from "./GameLogic";
+import GameResult from "./GameLogic";
 import "./GameTable.css";
 
 function GameTable() {
-  let sign = "";
-  //let tableArray = [100, 101, 102, 103, 104, 105, 106, 107, 108];
+  const [array, setArray] = useState([
+    100, 101, 102, 103, 104, 105, 106, 107, 108,
+  ]);
   const [player, setPlayer] = useState("Player 1");
   const handleClick = (id) => {
     if (player == "Player 1") {
       document.getElementById(`${id}`).innerHTML = "X";
+      setPlayer(array.splice(id, 1, "X"));
       console.log(player);
       setPlayer("Player 2");
     } else {
       document.getElementById(`${id}`).innerHTML = "0";
       console.log(player);
+      setPlayer(array.splice(id, 1, "0"));
       setPlayer("Player 1");
     }
+    GameResult(array);
+    console.log(array);
+    console.log(GameResult(array));
   };
   return (
     <>
+      <h1>TIC TAC TOE</h1>
       <div className="GameTable">
         <div onClick={() => handleClick(0)} id="0" className="grid-item"></div>
         <div onClick={() => handleClick(1)} id="1" className="grid-item"></div>
