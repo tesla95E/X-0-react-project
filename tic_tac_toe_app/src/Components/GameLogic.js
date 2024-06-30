3fd8fe676d0e9de4871548abff3f4ba2f50f40f8
tesla95E <andybiscom@gmail.com>
2024-06-30 21:37:41 +0300

    fixed some problems with GameLogic and add more context about the winner

--- tic_tac_toe_app/src/Components/GameLogic.js
+++ tic_tac_toe_app/src/Components/GameLogic.js
@@ -1,20 +1,52 @@
 export function GameResult(array) {
   if (array[0] == array[1] && array[0] == array[2]) {
-    return "player win";
-  } else if (array[1] == array[4] && array[3] == array[5]) {
-    return "player win";
+    if (array[0] == "X") {
+      return "A castigat Player 1";
+    } else {
+      return "A castigat Player 2";
+    }
+  } else if (array[3] == array[4] && array[3] == array[5]) {
+    if (array[3] == "X") {
+      return "A castigat Player 1";
+    } else {
+      return "A castigat Player 2";
+    }
   } else if (array[6] == array[7] && array[6] == array[8]) {
-    return "player win";
+    if (array[6] == "X") {
+      return "A castigat Player 1";
+    } else {
+      return "A castigat Player 2";
+    }
+  } else if (array[0] == array[3] && array[0] == array[6]) {
+    if (array[0] == "X") {
+      return "A castigat Player 1";
+    } else {
+      return "A castigat Player 2";
+    }
   } else if (array[1] == array[4] && array[1] == array[7]) {
-    return "player win";
+    if (array[1] == "X") {
+      return "A castigat Player 1";
+    } else {
+      return "A castigat Player 2";
+    }
   } else if (array[2] == array[5] && array[2] == array[8]) {
-    return "player win";
-  } else if (array[3] == array[6] && array[3] == array[9]) {
-    return "player win";
-  } else if (array[1] == array[5] && array[1] == array[9]) {
-    return "player win";
-  } else if (array[3] == array[5] && array[3] == array[7]) {
-    return "player win";
+    if (array[2] == "X") {
+      return "A castigat Player 1";
+    } else {
+      return "A castigat Player 2";
+    }
+  } else if (array[0] == array[4] && array[0] == array[8]) {
+    if (array[0] == "X") {
+      return "A castigat Player 1";
+    } else {
+      return "A castigat Player 2";
+    }
+  } else if (array[2] == array[4] && array[2] == array[6]) {
+    if (array[2] == "X") {
+      return "A castigat Player 1";
+    } else {
+      return "A castigat Player 2";
+    }
   } else {
     return "egal";
   }
